ee36654d0f94d38f1c9468224ba48bb3cac32ac0
Jethrolopwus <lopwusjethro92@gmail.com>
2024-12-28 15:50:52 +0100

    UPDATE: complete the basic ui design and routing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,58 +1,70 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../public/Frame 37142.png"
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white text-gray-800 py-8 border-t-2">
+    <footer className="py-8 border-t-2" id="contact">
       <div className="mx-10 py-4 px-4 md:px-8">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-          {/* Logo and Social Media */}
           <div className="flex flex-col items-center md:items-start">
-            <div className="mb-4 flex flex-row items-center">
+            <div className="mb-4 flex flex-row items-center text-3xl">
               <img
                 src={Logo}
                 alt="Delar"
                 className="w-12 h-12"
               />
-               <span className="text-blue-500 ml-3 hidden text-3xl md:block">DELAR</span>
+               <span className="text-blue-500 ml-3 hidden  md:block">DE</span>LAR
             </div>
             <div className="flex space-x-4">
               <a
-                href="https://facebook.com"
+                href="https://x.com/delarproject"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-gray-500 hover:text-blue-500"
+                className=" hover:text-blue-500"
               >
-                <i className="fab fa-facebook-f"></i>
+             
+                <FaXTwitter  size={30} />
+              </a>
+              <a
+                href="https://instagram"
+                target="_blank"
+                rel="noopener noreferrer"
+                className=" hover:text-blue-500"
+              >
+             
+             <FaInstagram  size={30}/>
               </a>
               <a
                 href="https://twitter.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-gray-500 hover:text-blue-500"
+                className=" hover:text-blue-500"
               >
                 <i className="fab fa-twitter"></i>
               </a>
               <a
-                href="https://instagram.com"
+                href="https://www.facebook.com/profile.php?id=100088954730570"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-gray-500 hover:text-blue-500"
+                className=" hover:text-blue-500"
               >
-                <i className="fab fa-instagram"></i>
+               <FaFacebookF size={30} />
               </a>
             </div>
           </div>
 
           {/* Company Links */}
           <div>
-            <h4 className="font-bold text-gray-900 mb-4">Company</h4>
+            <h4 className="font-bold mb-4">Company</h4>
             <ul className="space-y-2">
               <li>
                 <Link
                   to="/home"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Home
                 </Link>
@@ -60,7 +72,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/about"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   About
                 </Link>
@@ -68,7 +80,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/contact"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Contact Us
                 </Link>
@@ -76,7 +88,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/how-it-works"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   
                 </Link>
@@ -86,12 +98,12 @@ const Footer: React.FC = () => {
 
           {/* Product Links */}
           <div>
-            <h4 className="font-bold text-gray-900 mb-4">Product</h4>
+            <h4 className="font-bold mb-4">Product</h4>
             <ul className="space-y-2">
               <li>
                 <Link
                   to="/testimonials"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Testimonials
                 </Link>
@@ -99,7 +111,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/pricing"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Pricing
                 </Link>
@@ -107,7 +119,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/features"
-                  className="text-gray-600 hover:text-blue-500"
+                  className="t hover:text-blue-500"
                 >
                   Features
                 </Link>
@@ -115,7 +127,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/request-demo"
-                  className="text-gray-600 hover:text-blue-500"
+                  className="t hover:text-blue-500"
                 >
                   Request Demo
                 </Link>
@@ -125,12 +137,12 @@ const Footer: React.FC = () => {
 
           {/* Legals & Resources */}
           <div>
-            <h4 className="font-bold text-gray-900 mb-4">Legals</h4>
+            <h4 className="font-bold  mb-4">Legals</h4>
             <ul className="space-y-2">
               <li>
                 <Link
                   to="/privacy-policy"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Privacy Policy
                 </Link>
@@ -138,7 +150,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/terms-of-service"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Terms of Service
                 </Link>
@@ -146,19 +158,19 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/cookie-policy"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Cookie Policy
                 </Link>
               </li>
             </ul>
 
-            <h4 className="font-bold text-gray-900 mt-8 mb-4">Resources</h4>
+            <h4 className="font-bold  mt-8 mb-4">Resources</h4>
             <ul className="space-y-2">
               <li>
                 <Link
                   to="/documentation"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   Documentation
                 </Link>
@@ -167,7 +179,7 @@ const Footer: React.FC = () => {
               <li>
                 <Link
                   to="/faqs"
-                  className="text-gray-600 hover:text-blue-500"
+                  className=" hover:text-blue-500"
                 >
                   FAQs
                 </Link>
@@ -177,7 +189,7 @@ const Footer: React.FC = () => {
         </div>
 
         {/* Footer Bottom Text */}
-        <div className="mt-8 text-center text-sm text-gray-500">
+        <div className="mt-8 text-center text-sm ">
           Copyright © {new Date().getFullYear()} Delar.com
         </div>
       </div>
--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,24 +1,23 @@
-import Benefits from "./Benefits"
-import Footer from "./Footer"
+
 import Navbar from "./Navbar"
 
 const HeroComponent = () => {
   return (
-    <div className="bg-[#000F24] min-h-screen">
+    <div className=" min-h-screen" id="/">
       <Navbar/>
-    <section className="flex flex-col items-center justify-center text-center h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-white">
-      <h1 className="text-4xl md:text-5xl font-bold mb-4">
+    <section className="flex flex-col items-center  justify-center text-center h-screen pt-32">
+      <h1 className="text-4xl md:text-5xl font-bold mb-8 ">
       The Decentralized Land Registry{" "}
         <span className="text-blue-500">Delar</span>.
       </h1>
       <p className="text-lg md:text-xl max-w-2xl mb-8">
       leverages blockchain technology to create a transparent, secure and immutable record of land transactions. This platform ensures that landowners, buyers and local authorities can interact within a trustless environment, reducing the incidence of fraud and land-related disputes.
       </p>
-      <div>
+      <div className="pt-10">
         <button className="bg-blue-500 text-white px-6 py-3 rounded-md mr-4 hover:bg-blue-600">
           Get Started
         </button>
-        <button className="border border-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-500">
+        <button className="border border-blue-500  px-6 py-3 rounded-md hover:bg-blue-500 mb-8">
           Book a Demo
         </button>
       </div>
